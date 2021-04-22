3d2a7a4cfca86244a696b0c95daf43311842671d
kory-job <catcher.intherye.rk@gmail.com>
2021-04-22 11:52:29 +0900

    chapter12完了

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :staff do
+  namespace :staff, path: "" do
     root "top#index"
     get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destory"
+    resource :session, only: [:create, :destroy]
+    resource :account, except: [:new, :create, :destory]
   end
 
   namespace :admin do
     root "top#index"
     get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destory"
+    resource :session, only: [:create, :destroy]
+    resources :staff_members
   end
 
   namespace :customer do
